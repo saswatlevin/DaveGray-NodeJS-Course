7f368e1d0c9303a75b1737e11e96c7c5bbf07e8d
Saswat Levin <saswatlevin@gmail.com>
2024-10-20 01:19:23 +0530

    Moved img, css and data.txt to public folder

--- server.js
+++ server.js
@@ -1,211 +1,67 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-const logEvents = require('./logEvents.js');
-const EventEmitter = require('events');
-
-
-class MyEmitter extends EventEmitter {};
-
-// Initialize object
-const myEmitter = new MyEmitter();
-
-// Add listener for the log event
-// Here, we first specify the event type('log') thet we're listening for
-// followed by an anon fn to pass the message to the logEvents fn
-myEmitter.on('log', (msg) => logEvents(msg));
-
-// Here, we create the emitter function
-// and set a delay of 2000 ms
-setTimeout(() => {
-    // Emit event
-    myEmitter.emit('log', 'Log event emitted');
-}, 2000);
-
-// Server Code
+const express = require("express");
+const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3500;
 
+//Express.js works like a waterfall. It first goes to a route '/', then to the route with '/index.html' and so on
+// Wildcards can be included in the routes
+// '^/'    -> Starting with a /
+// '$|/'   -> Ending with a /
+// (text)? -> Doesn't include 'text'
 
-/* const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+// All the app.get(), app.post(), etc. are called Route Handlers. 
 
-    // Undefined variable 
-    let filePath;
-    // Logic to serve page
-    if (req.url === '/' || req.url === 'index.html') {
-        // Set Status Code
-        res.statusCode = 200;
-        // Set HTTP Header Content
-        res.setHeader('Content-Type', 'text/html');
-        filePath = path.join(__dirname, 'views', 'index.html');
-        // Read File
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            res.end(data);
-        });
-    }
-}); */
+// Middleware is anything that goes between the request and the response.
+// Route handlers are middleware.
+// There are 3 types of middleware: built-in, custom and 3rd-party.
+// app.use() is often used to apply middleware.
 
-// File serving function
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        // Supports images and also text / html / json / etc data
-        var encoding;
+// Middleware also works like a waterfall. So, if we apply any middleware in a line that's above
+// all routes, so that middleware will apply to all routes.
 
-        if (contentType.includes('image') == true) {
-            encoding = '';
-        }
+/** MIDDLEWARE */
 
-        else {
-            encoding = 'utf8';
-        }
+// Built-in middleware to handle form data (url encoded; "application/x-www-form-urlencoded")
+app.use(express.urlencoded({extended: false}));
 
-        console.log("Content Type:", contentType, "encoding:", encoding);
+// Built-in middleware for JSON
+app.use(express.json());
 
-        const rawData = await fsPromises.readFile(
-            filePath, 
-            encoding
-        );
-        
-        // Parse the data if it is of type jso, otherwise not
-        const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+// Serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
-        // Send a response header to the incoming request
-        response.writeHead(
-            // To send status as 404 when such a request is processed from here.
-            filePath.includes("404.html") ? 404 : 200, 
-            {'Content-Type': contentType}
-        );
-        
-        // End the response by sending data
-        response.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        );
-
-        console.log("==========================END==========================\n");
-
-    } catch (err) {
-        console.log(err);
-        response.statusCode = 500;
-        response.end();
-    }
-}
-
-const server = http.createServer((req, res) => {
-    console.log("URL->", req.url, "METHOD->", req.method);
-
-    // Get the path from the extension name
-    const extension = path.extname(req.url);
-    console.log("extension:", extension);
-    
-    let contentType;
-
-    // Determine the content type based on file extension
-    switch(extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        
-        case '.json':
-            contentType = 'application/json';
-            break;
-        
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-
-        case '.jpeg':
-            contentType = 'image/jpeg';
-            break;
-
-        case '.png':
-            contentType = 'image/png';
-            break;
-
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        
-        default:
-            contentType = 'text/html';
-    }
-
-    console.log("Content Type:", contentType);
-
-    if (contentType === 'text/html' && req.url === '/' ) {
-        filePath = path.join(__dirname, 'views', 'index.html');
-        console.log("filePath", filePath);
-        //console.log("\n");
-    }
-
-    // The slice(-1) refers to the last character in the requested URL
-    else if (contentType === 'text/html' && req.url.slice(-1) === '/') {
-        // The req.url provides the path to the "subdir" folder
-        filePath = path.join(__dirname, 'views', req.url, 'index.html');
-        console.log("filePath", filePath);
-        //console.log("\n");
-    }
-
-    else if (contentType === 'text/html') {
-       filePath = path.join(__dirname, 'views', req.url);
-       console.log("filePath", filePath);
-       //console.log("\n");
-    }
-
-    else {
-        filePath = path.join(__dirname, req.url);
-        console.log("filePath", filePath);
-        //console.log("\n");
-    }
-
-
-    if (!extension && req.url.slice(-1) !== '/') {
-        console.log("filePath", filePath);
-        //console.log("\n");
-        filePath = filePath + ".html";
-    }
-
-    // Returns a boolean value
-    const fileExists = fs.existsSync(filePath);
-
-
-    /** FILE SERVING LOGIC **/
-    if (fileExists) {
-        // Serve the file
-        serveFile(filePath, contentType, res);
-    }
-
-    else {
-        
-        // Serve a 301 (redirect) response
-        switch(path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, {'Location': '/new-page.html'});
-                // End the response w/o sending data
-                res.end();
-                console.log("==========================END301==========================\n");
-                break;
-
-            case 'www-page.html':
-                res.writeHead(301, {'Location': '/'});
-                // End the response w/o sending data
-                res.end();
-                console.log("==========================END301==========================\n");
-                break;
-
-            default:
-                // Serve a 404 (not found) response
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-                console.log("==========================END404==========================\n");
-        }
-        //console.log(path.parse(filePath));
-    }
+// This will allow us to route to 'index', '/index' and '/index/' with or without the '.html'
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Listen for incoming requests.
-server.listen(PORT, () => console.log("Server running on port ", PORT));
-console.log("\n");
+// Route to /new-page or /new-page.html
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Does a permanent redirect to 'new-page.html'
+// If the 301 wasn't specified, by default, a search engine would do a temporary redirect (302) 
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, 'new-page.html');
+});
+
+// The next() function directs the flow of control to the next route
+// Here, after trying to load old-page.html, the next arrow fn inside it would be executed.
+// If that arrow fn wasn't there, then, we would be directed to the 404.html page due to the last route.
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('Attempted to load hello.html');
+    next();
+
+}, (req, res) => {
+    res.send('Hello World');
+});
+
+// Redirects any url other than 'index', 'new-page' or 'old-page' to the 404.html page with status code 404.
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(PORT, () => console.log("Server running on port", PORT));
+
+
