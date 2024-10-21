437da78fccf34959e16a90236965c924a9b3d1b4
Saswat Levin <saswatlevin@gmail.com>
2024-10-22 00:41:44 +0530

    Separated the controllers from the employees.js file

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,42 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const employeesController = require('../../controllers/employeesController');
 
-const data = {};
-data.employees = require('../../model/employees.json');
 
 // The get(), post(), put() and delete() methods are chained.
 // They all belong to the same route
 router.route('/')
-    .get((req, res) => {
-        // Send the json as such
-        res.json(data.employees);
-    })
-    .post((req, res) => {
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname 
-        });
-    })
-    .put((req, res) => {
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname 
-        });
-    })
-    .delete((req, res) => {
-        res.json({
-            "id": req.body.id
-        });
-    });
+    .get(employeesController.getAllEmployees)
+    .post(employeesController.createNewEmployee)
+    .put(employeesController.updateEmployee)
+    .delete(employeesController.deleteEmployee);
 
 // Route to get an employee by id
 router.route('/:id')
-    .get((req, res) => {
-        res.json({
-            "id": req.params.id
-        });
-    });    
+    .get(employeesController.getSingleEmployee);    
 
 
 module.exports = router;
