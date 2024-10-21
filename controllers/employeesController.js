0050a4aeb80635b75dac9130dca9bde168efd13a
Saswat Levin <saswatlevin@gmail.com>
2024-10-22 00:39:33 +0530

    Added first version of controllers

---
+++ controllers/employeesController.js
@@ -0,0 +1,41 @@
+const data = {};
+data.employees = require('../model/employees.json');
+
+const getAllEmployees = (req, res) => {
+    // Send the json as such
+    res.json(data.employees);
+};
+
+const createNewEmployee = (req, res) => {
+    res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname 
+    });
+};
+
+const updateEmployee = (req, res) => {
+    res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname 
+    });
+};
+
+const deleteEmployee = (req, res) => {
+    res.json({
+        "id": req.body.id
+    });
+}
+
+const getSingleEmployee = (req, res) => {
+    res.json({
+        "id": req.params.id
+    });
+}
+
+module.exports = {
+    getAllEmployees,
+    createNewEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getSingleEmployee
+}
